a075bd3649e39a80c4576211235822caa22c1dca
Anas Abdul <anasabdulwahid@gmail.com>
2021-07-30 21:17:25 +0800

    update

---
+++ screens/AccountScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { commonStyles } from "../styles/commonStyles";
+
+export default function AccountScreen({ navigation }) {
+  function signOut() {}
+ 
+
+  return (
+    <View style={commonStyles.container}>
+      <Text>Account Screen</Text>
+      <Button title="Sign out" onPress={signOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
